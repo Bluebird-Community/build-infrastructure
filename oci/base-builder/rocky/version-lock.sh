e0d53fac04c16dc0f276cca58427fd5390fe314f
Ronny Trommer <ronny@no42.org>
2025-01-20 14:00:25 +0100

    chore: Update base builder image

--- oci/base-builder/rocky/version-lock.sh
+++ oci/base-builder/rocky/version-lock.sh
@@ -7,7 +7,7 @@ DATE="$(date -u +"%Y-%m-%dT%H:%M:%SZ")"
 export VCS_SOURCE
 export VCS_REVISION
 export DATE
-export BASE_IMAGE="quay.io/rockylinux/rockylinux:9-ubi"
+export BASE_IMAGE="quay.io/rockylinux/rockylinux:9.5.20241118-ubi"
 export HADOLINT_VERSION="2.12.0"
 export CLOUDSMITH_CLI_VERSION="1.4.1"
 export FPM_VERSION="1.15.1"
