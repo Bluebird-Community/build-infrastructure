81c53b4e44a78223fd9bfdd59fcbc42e733d512e
Ronny Trommer <ronny@no42.org>
2025-09-26 23:26:12 +0200

    chore: Update iplike builder
    Signed-off-by: Ronny Trommer <ronny@no42.org>

--- oci/iplike-builder/version-lock.sh
+++ oci/iplike-builder/version-lock.sh
@@ -7,4 +7,4 @@ DATE="$(date -u +"%Y-%m-%dT%H:%M:%SZ")"
 export VCS_SOURCE
 export VCS_REVISION
 export DATE
-export BASE_IMAGE="quay.io/bluebird/base-builder:ubuntu.0.1.8.b20"
+export BASE_IMAGE="quay.io/bluebird/base-builder:ubuntu.0.1.9.b21"
